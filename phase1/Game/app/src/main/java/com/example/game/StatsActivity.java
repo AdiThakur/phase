f893439b1654c3142242bd0c6628ccffa91e5181
Luke Bacchus <luke.bacchus@mail.utoronto.ca>
2019-10-31 16:44:46 -0400

    "Implemented stats activity and corresponding layout file to display current user's relevant stats."

---
+++ phase1/Game/app/src/main/java/com/example/game/StatsActivity.java
@@ -0,0 +1,37 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class StatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("user");
+        User user = new DataLoader(this).loadUser(userName);
+
+        // Connect Stats
+        TextView connectGamesPlayed = findViewById(R.id.connectGamesPlayed);
+        connectGamesPlayed.setText(user.connectStats.getGamesPlayed() + "");
+        TextView connectTimePlayed = findViewById(R.id.connectTimePlayed);
+        connectTimePlayed.setText(user.connectStats.getTimePlayed() + "");
+        TextView connectGamesWon = findViewById(R.id.connectGamesWon);
+        connectGamesWon.setText(user.connectStats.getGamesWon() + "");
+
+        // Match Stats
+        TextView repeatGamesPlayed = findViewById(R.id.repeatGamesPlayed);
+        repeatGamesPlayed.setText(user.matchStats.getGamesPlayed()+ "");
+        TextView repeatTimePlayed = findViewById(R.id.repeatTimePlayed);
+        repeatTimePlayed.setText(user.matchStats.getTimePlayed() + "");
+        TextView repeatMistakes = findViewById(R.id.repeatMistakes);
+        repeatMistakes.setText(user.matchStats.getTotalMistakes() + "");
+
+    }
+}
