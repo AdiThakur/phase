caf40102de0ec8ccc0078ccc215a96bd9cb0adcc
Luke Bacchus <luke.bacchus@mail.utoronto.ca>
2019-10-31 23:07:05 -0400

    "Finished stats activity and corresponding layout file to display current user's relevant stats for each game."

--- phase1/Game/app/src/main/java/com/example/game/StatsActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/StatsActivity.java
@@ -25,13 +25,21 @@ public class StatsActivity extends AppCompatActivity {
         TextView connectGamesWon = findViewById(R.id.connectGamesWon);
         connectGamesWon.setText(user.connectStats.getGamesWon() + "");
 
-        // Match Stats
+        // Repeat Stats
         TextView repeatGamesPlayed = findViewById(R.id.repeatGamesPlayed);
         repeatGamesPlayed.setText(user.matchStats.getGamesPlayed()+ "");
         TextView repeatTimePlayed = findViewById(R.id.repeatTimePlayed);
         repeatTimePlayed.setText(user.matchStats.getTimePlayed() + "");
         TextView repeatMistakes = findViewById(R.id.repeatMistakes);
         repeatMistakes.setText(user.matchStats.getTotalMistakes() + "");
+
+        // Guess Stats
+        TextView guessGamesPlayed = findViewById(R.id.guessGamesPlayed);
+        guessGamesPlayed.setText(user.guessStats.getGamesPlayed()+ "");
+        TextView guessTimePlayed = findViewById(R.id.guessTimePlayed);
+        guessTimePlayed.setText(user.guessStats.getTimePlayed() + "");
+        TextView guessLongestStreak = findViewById(R.id.guessLongestStreak);
+        guessLongestStreak.setText(user.guessStats.getLongestStreak() + "");
 
     }
 }
